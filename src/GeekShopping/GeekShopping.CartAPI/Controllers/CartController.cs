651ee6982c0a9a780af2c39338053971be745825
GuiBarbosa23 <guibarbosadeveloper@gmail.com>
2022-05-18 15:04:03 -0300

    Implementação do microserviço de carrinho de compras

---
+++ src/GeekShopping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CartAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private ICartRepository _cartRepository;
+
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+        }
+
+        [HttpGet("find-cart/{id}")]
+        public async Task<ActionResult<CartVO>> FindById(string id)
+        {
+            var product = await _cartRepository.FindCartByUserId(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost("add-cart")]
+        public async Task<ActionResult<CartVO>> AddCart(CartVO vo)
+        {
+            var product = await _cartRepository.SaveOrUpdateCart(vo);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+        [HttpPut("update-cart")]
+        public async Task<ActionResult<CartVO>> UpdateCart(CartVO vo)
+        {
+            var product = await _cartRepository.SaveOrUpdateCart(vo);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpDelete("remove-cart/{id}")]
+        public async Task<ActionResult<CartVO>> RemoveCart(long id)
+        {
+            var status = await _cartRepository.RemoveFromCart(id);
+            if (!status) return NotFound();
+            return Ok(status);
+        }
+    }
+}
